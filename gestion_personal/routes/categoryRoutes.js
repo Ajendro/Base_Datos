39ca7109a5b9f4e4bc7decb27892696880447098
SiomaraCG <desireesiomara@gmail.com>
2024-06-28 23:42:06 -0500

    sub

--- gestion_personal/routes/categoryRoutes.js
+++ gestion_personal/routes/categoryRoutes.js
@@ -6,12 +6,14 @@ import {
   getCategorys,
   updateCategory,
   deleteCategory,
+  createSubCategory,
 } from '../controllers/categoryControllers.js';
 
 const router = express.Router();
 
 router.get('/categories', getCategorys);
 router.post('/new_category', createCategory);
+router.post('/new_sub_category', createSubCategory);
 router.get('/category/:id', getCategory);
 router.put('/update_category/:id', updateCategory);
 router.delete('/delete_category/:id', deleteCategory);
